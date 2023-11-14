7ff433f00b6f724f3e8dec08e73287bd96e29e83
ngM0de <tillic0llapse@protonmail.com>
2023-11-15 00:48:27 +0200

    done

---
+++ db.js
@@ -0,0 +1,16 @@
+import mongoose, {mongo} from "mongoose";
+
+
+export const mongoConnect = async () => {
+    return await mongoose.connect(process.env.MONGO_URI.replace('<password>', encodeURIComponent(process.env.MONGO_PASSWORD)), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+}
+
+const urlSchema = new mongoose.Schema({
+    original_url: String,
+    short_url: String
+})
+
+export const UrlsModel = mongoose.model('urls', urlSchema, 'short_urls')
--- index.js
+++ index.js
@@ -1,24 +1,63 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+import dotenv from "dotenv"
+import express from "express";
+import cors from "cors"
+import {nanoid} from "nanoid"
+import bodyParser from "body-parser";
+import cookieParser from 'cookie-parser'
+import {mongoConnect, UrlsModel} from "./db.js";
+import urlValidator from "valid-url"
 
-// Basic Configuration
+dotenv.config()
+
+
+mongoConnect().then().catch(console.log)
+
+const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(cors());
+app.use(cookieParser())
+app.use(bodyParser.urlencoded())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', function (req, res) {
+    res.json({greeting: 'hello API'});
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.post('/api/shorturl', (req, res) => {
+    const isUrlValid = urlValidator.isHttpUri(req.body.url) || urlValidator.isHttpsUri(req.body.url);
+    console.log(isUrlValid)
+    if (isUrlValid) {
+        const response = {original_url: req.body.url, short_url: nanoid(4)}
+        const payload = new UrlsModel(response)
+        payload.save().then(queryResult => {
+            res.json(response)
+        }).catch(queryResult => {
+            res.json(queryResult)
+        })
+    } else {
+        res.json({error: "invalid url"})
+    }
+
+
+})
+
+app.get('/api/shorturl/:shorturl', (req, res) => {
+    UrlsModel.findOne({short_url: req.params.shorturl}).then(queryResult => {
+        res.redirect(queryResult.original_url);
+    }).catch(queryResult => {
+        res.json(queryResult)
+    })
+
+})
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
